b18059c09a37ed0803520291ff6e66408ad85398
MIM-104 <134318051+MIM-104@users.noreply.github.com>
2024-11-10 00:34:34 +0000

    Rayfield.lua

--- Rayfield.lua
+++ Rayfield.lua
@@ -211,7 +211,7 @@ if RunService:IsStudio() then
 end
 
 -- Interface Management
-local Rayfield = useStudio and script.Parent:FindFirstChild('Rayfield') or game:GetObjects("rbxassetid://134415566685128")[1]
+local Rayfield = useStudio and script.Parent:FindFirstChild('Rayfield') or game:GetObjects("rbxassetid://138479981673371")[1]
 
 Rayfield.Enabled = false
 
